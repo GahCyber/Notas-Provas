cbdf790fdbb4a5d0e065f5ad31ec8ec66eb14aad
Gabriel Gedolin <gabrielgedoli@gmail.com>
2025-02-25 18:25:09 -0300

    mudei o title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Boletim Escolar</title>
+        <title>Boletim</title>
       </Head>
       <Component {...pageProps} />
     </>
